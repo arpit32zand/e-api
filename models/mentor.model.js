0ae6d75ce211fb6444d20344a0461d8cfb4cd87a
arpit32zand <arpit1999zandrew@gmail.com>
2020-04-07 11:07:21 +0530

    Logout App-side Changes

--- models/mentor.model.js
+++ models/mentor.model.js
@@ -4,48 +4,70 @@ const memtor = new Schema({
   uid: {
     type: Number,
     required: true,
-    trim: true
+    trim: true,
   },
   mid: {
     type: Number,
     required: true,
     //minlength: 5,
     trim: true,
-    unique: true
+    unique: true,
   },
   email: {
     type: String,
     required: true,
     minlength: 7,
     trim: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: true,
     minlength: 7,
     trim: true,
-    unique: true
+    unique: true,
   },
   mobileno: {
     type: Number,
     minlength: 10,
     maxlength: 10,
-    trim: true
-  },
-  subject: {
-    type: String,
-    required: true,
-    minlength: 7,
     trim: true,
-    unique: true
   },
+  subject: [
+    {
+      courseId: {
+        type: Number,
+        //required: true,
+        //minlength: 5,
+        trim: true,
+        unique: true,
+      },
+      courseName: {
+        type: String,
+        //required: true,
+        //minlength: 5,
+        trim: true,
+      },
+      fileType: {
+        type: String,
+        //required: true,
+        //minlength: 5,
+        trim: true,
+      },
+      path: {
+        type: String,
+        //required: true,
+        //minlength: 5,
+        trim: true,
+      },
+    },
+  ],
   username: {
     type: String,
     required: true,
     minlength: 5,
-    trim: true
-  }
+    trim: true,
+  },
 });
 const Mentor = mongoose.model("Mentor", memtor);
 module.exports = Mentor;
--- routers/users.js
+++ routers/users.js
@@ -3,11 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcryptjs");
-const cookieParser = require("cookie-parser");
+//const cookieParser = require("cookie-parser");
 const router = express.Router();
+const session = require("express-session");
 require("dotenv").config();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 const User = require("../models/user.model");
 const Test = require("../models/test.model");
 const Mentor = require("../models/mentor.model");
@@ -16,9 +17,23 @@ const Candidate = require("../models/candidate.model");
 const saltRounds = 10;
 // let uid = process.env.UID;
 
+const IN_PROD = process.env.NODE_ENV === "production";
 let mes = "";
 let ra = "";
-router.use(cookieParser());
+//router.use(cookieParser());
+router.use(
+  session({
+    name: process.env.SESS_NAM,
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SEC_CODE,
+    cookie: {
+      maxAge: 1000 * 5,
+      sameSite: true,
+      secure: false,
+    },
+  })
+);
 
 //sign-up
 router.route("/add").post((req, res) => {
@@ -36,17 +51,17 @@ router.route("/add").post((req, res) => {
         requireTLS: true,
         auth: {
           user: "eguru.proj@gmail.com",
-          pass: "pehelahai1"
-        }
+          pass: "pehelahai1",
+        },
       });
       var mailOptions = {
         from: "eguru.proj@gmail.com",
         to: email,
         subject: "no-reply",
-        text: "Please Enter This Verification Code to Register \n" + mes
+        text: "Please Enter This Verification Code to Register \n" + mes,
       };
 
-      transporter.sendMail(mailOptions, function(error, info) {
+      transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
           console.log("Mailer error : ", error);
         } else {
@@ -56,7 +71,6 @@ router.route("/add").post((req, res) => {
 
       res.send({
         result: "Send",
-        details: req.cookies.userDetails
       });
     } else {
       res.send({ result: "Account Already Exist!" });
@@ -80,7 +94,7 @@ router.route("/verify").post((req, res) => {
 
   let uid;
   if (code === mes) {
-    bcrypt.hash(password, saltRounds, function(err, hash) {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
       if (err) {
         console.log(err);
       } else {
@@ -91,7 +105,7 @@ router.route("/verify").post((req, res) => {
           const newUser = new User({ uid, username, email, password });
           newUser.save().then(() => {
             Mentor.countDocuments()
-              .then(count => {
+              .then((count) => {
                 ra = count + 1;
                 const newAcc = new Mentor({
                   uid,
@@ -99,15 +113,23 @@ router.route("/verify").post((req, res) => {
                   username,
                   email,
                   password,
-                  mobileno
+                  mobileno,
+                  subject: [
+                    {
+                      courseId,
+                      courseName,
+                      fileType,
+                      path,
+                    },
+                  ],
                 });
 
                 newAcc
                   .save()
                   .then(() => res.send({ result: "Created" }))
-                  .catch(err => res.status(400).json("error: " + err));
+                  .catch((err) => res.status(400).json("error: " + err));
               })
-              .catch(err => res.status(400).json("error: " + err));
+              .catch((err) => res.status(400).json("error: " + err));
           });
         } else {
           uid = 2;
@@ -115,7 +137,7 @@ router.route("/verify").post((req, res) => {
           newUser
             .save()
             .then(() => {
-              Candidate.countDocuments().then(count => {
+              Candidate.countDocuments().then((count) => {
                 ra = count + 1;
                 const newAcc = new Candidate({
                   uid,
@@ -129,17 +151,17 @@ router.route("/verify").post((req, res) => {
                       courseId,
                       courseName,
                       fileType,
-                      path
-                    }
-                  ]
+                      path,
+                    },
+                  ],
                 });
                 newAcc
                   .save()
                   .then(() => res.send({ result: "Created" }))
-                  .catch(err => res.status(400).json("error: " + err));
+                  .catch((err) => res.status(400).json("error: " + err));
               });
             })
-            .catch(err => res.status(400).json("error: " + err));
+            .catch((err) => res.status(400).json("error: " + err));
         }
       }
     });
@@ -165,17 +187,17 @@ router.route("/forgotmail").post((req, res) => {
         requireTLS: true,
         auth: {
           user: "eguru.proj@gmail.com",
-          pass: "pehelahai1"
-        }
+          pass: "pehelahai1",
+        },
       });
       var mailOptions = {
         from: "eguru.proj@gmail.com",
         to: email,
         subject: "no-reply",
-        text: "Click on the link Given below \n" + uurl
+        text: "Click on the link Given below \n" + uurl,
       };
 
-      transporter.sendMail(mailOptions, function(error, info) {
+      transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
           console.log(error);
         } else {
@@ -193,7 +215,7 @@ router.route("/forgotpass").put((req, res) => {
   let newpass = req.body.newpass;
   const confpass = req.body.confpass;
   if (newpass === confpass) {
-    bcrypt.hash(newpass, saltRounds, function(err, hash) {
+    bcrypt.hash(newpass, saltRounds, function (err, hash) {
       if (err) {
         console.log(err);
       } else {
@@ -214,6 +236,7 @@ router.route("/forgotpass").put((req, res) => {
 router.route("/").post((req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  let sessUser = "";
 
   User.findOne({ email: email }, (err, foundAcc) => {
     if (err) console.log(err);
@@ -222,114 +245,32 @@ router.route("/").post((req, res) => {
       bcrypt.compare(password, foundAcc.password, (err, accre) => {
         if (err) console.log(err);
         else {
-          if (accre == true)
-            if (foundAcc.category === "teacher")
-              res.send({ result: "TLogged" });
-            else res.send({ result: "SLogged" });
-          else res.send({ result: "Wrong Password" });
+          if (accre == true) {
+            if (foundAcc.uid === 1) {
+              res.send({ result: "TLogged", sessUser });
+            } else res.send({ result: "SLogged", sessUser });
+          } else res.send({ result: "Wrong Password" });
         }
       });
     } else res.send({ result: "Wrong Email" });
   });
 });
 
-router.route("/send").post((req, res) => {
-  const user = req.body.user;
-  // Test.findOneAndUpdate(
-  //   (err, data) => {
-  //     if (err) console.log(err);
-  //     if (data) {
-  //       console.log({ result: "Success" });
-  //     }
-  //   }
-  // );
-  let ra = "";
-  Test.countDocuments().then(count => {
-    ra = count + 1;
-    const newUser = new Test({ uid: ra, user });
-
-    newUser
-      .save()
-      .then(() => res.send({ result: "Created" }))
-      .catch(err => res.status(400).json("error: " + err));
+router.route("/logout").delete((req, res) => {
+  req.session.destroy((err) => {
+    if (err) throw err;
+    res.clearCookie(SESS_NAM).send({ result: "OUT" }); // clears cookie containing expired sessionID
   });
 });
-router.route("/send").get((req, res) => {
-  /*res.send([
-    {
-      email: "arpit1999@gmail.com",
-      uname: "Arpit"
-    },
-    {
-      email: "ankur@gmail.com",
-      uname: "Ankur"
-    },
-    {
-      uname: req.body.uname
-    }
-  ]);*/
 
-  res.send(req.cookies.testcook);
+router.route("/authchecker").get((req, res) => {
+  const sessUser = req.session.email;
+  if (sessUser) {
+    res.sendFile("test.html", { root: "../e-app/" });
+    // res.redirect("/test");
+  } else {
+    res.sendFile("LogIn.html", { root: "../e-app/" });
+  }
 });
 
 module.exports = router;
-
-/*if (newpassword === confirmpassword) {
-  User.findOne({ email: email }, (err, foundData) => {
-    if (err) {
-      res.status((400).json("error: " + err));
-    } else {
-      foundData.password = newpassword;
-    }
-
-    foundData.save((err, data) => {                                 
-      if (err) console.log(err);
-      else res.send("Password Changed Successfully");
-    });
-  });
-} else {
-  res.send("New Password Did Not MATCH Confirm Password");
-}*/
-
-//add-new
-/*router.route("/add").post((req, res) => {
-  const mes = req.body.vcode;
-  const email = req.body.email;
-
-  User.findOne({ email: email }, (err, foundAcc) => {
-    if (err) console.log(err);
-
-    if (!foundAcc) {
-      var transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        requireTLS: true,
-        auth: {
-          user: "eguru.proj@gmail.com",
-          pass: "pehelahai1"
-        }
-      });
-      var mailOptions = {
-        from: "eguru.proj@gmail.com",
-        to: email,
-        subject: "no-reply",
-        text: "Please Enter This Verification Code to Register \n" + mes
-      };
-
-      transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Send!");
-        }
-      });
-
-      res.send({
-        result: "Send",
-      });
-    } else {
-      res.send({ result: "Account Already Exist!" });
-    }
-  });
-});*/
